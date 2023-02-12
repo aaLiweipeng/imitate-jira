30c0bff7951f664770f6c1f3886b69580989a7e9
aaLiweipeng <31944741+aaLiweipeng@users.noreply.github.com>
2023-02-13 00:36:39 +0800

    feat: 用cssinjs封装通用组件

---
+++ src/components/lib.tsx
@@ -0,0 +1,69 @@
+/**
+ * 通用组件封装集
+ */
+import styled from "@emotion/styled";
+import { Button, Spin, Typography } from "antd";
+// import { DevTools } from "jira-dev-tool";
+
+export const Row = styled.div<{
+  gap?: number | boolean;
+  between?: boolean;
+  marginBottom?: number;
+}>`
+  display: flex;
+  align-items: center;
+  justify-content: ${(props) => (props.between ? "space-between" : undefined)};
+  margin-bottom: ${(props) => props.marginBottom + "rem"};
+
+  > * {
+    margin-top: 0 !important;
+    margin-bottom: 0 !important;
+    margin-right: ${(props) =>
+      typeof props.gap === "number"
+        ? props.gap + "rem" // 是number实值的，加个rem
+        : props.gap
+        ? "2rem" // 没有number实值，但是为 true，取默认值
+        : undefined};
+  }
+`;
+
+const FullPage = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const FullPageLoading = () => (
+  <FullPage>
+    <Spin size={"large"} />
+  </FullPage>
+);
+
+// export const FullPageErrorFallback = ({ error }: { error: Error | null }) => (
+//   <FullPage>
+//     <DevTools />
+//     <ErrorBox error={error} />
+//   </FullPage>
+// );
+
+// 类型守卫
+const isError = (value: any): value is Error => value?.message;
+
+export const ErrorBox = ({ error }: { error: unknown }) => {
+  if (isError(error)) {
+    return <Typography.Text type={"danger"}>{error?.message}</Typography.Text>;
+  }
+  return null;
+};
+
+export const ButtonNoPadding = styled(Button)`
+  padding: 0;
+`;
+
+export const ScreenContainer = styled.div`
+  padding: 3.2rem;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
